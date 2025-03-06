68af6cdf5d1b2a7610eaeca236b5e545c7efa478
GUSTAVO MIGUEL DOS SANTOS PEREIRA <24122458@dsn127m1246733.cfp127aluno.local>
2025-03-06 09:37:36 -0300

    okk

---
+++ app/src/main/java/br/senai/sp/jandira/bmi/screens/HomeScreen.kt
@@ -0,0 +1,160 @@
+package br.senai.sp.jandira.bmi.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.bmi.R
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+   
+   var nameState = remember  {
+       mutableStateOf("")
+
+   }
+   
+
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.horizontalGradient(
+                    listOf(
+                        Color(0xFF0F5711),
+                        Color(0xFF29772C),
+                        Color(0xFF3FC747),
+                        Color(0xFF84DC89),
+                        Color(0xFF3FC747),
+                        Color(0xFF29772C),
+                        Color(0xFF0F5711),
+                    )
+                )
+            )
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(
+                    R.drawable.football
+                ),
+                contentDescription = stringResource(
+                    R.string.logo
+                ),
+                modifier = Modifier
+                    .padding(top = 32.dp)
+            )
+            Text(
+                text = stringResource(
+                    R.string.welcome
+                ),
+                color = Color.Green,
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(400.dp),
+
+                shape = RoundedCornerShape(
+                    topStart = 48.dp,
+                    topEnd = 48.dp
+                ),
+                colors = CardDefaults
+                    .cardColors(
+                        containerColor = Color.White
+                    )
+
+            ){
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(32.dp),
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    horizontalAlignment = Alignment.End
+                ) {
+                    Column (
+                        modifier = Modifier
+
+                            .fillMaxWidth()
+                    ){
+                        Text(
+                            text = stringResource(
+                                R.string.your_name
+
+                            ),
+                            fontSize = 24.sp
+                        )
+                        TextField(
+                            value = nameState.value,
+                            onValueChange = {
+                            nameState.value = it
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 8.dp),
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Text,
+                                capitalization = KeyboardCapitalization.Words
+                            )
+
+                        )
+                    }
+                    Button(
+                        onClick = {},
+                        shape = RoundedCornerShape(8.dp)
+                    ) {
+                        Text(
+                            text = stringResource(
+                                R.string.next
+                            ),
+                            fontSize = 22.sp,
+                        )
+                    }
+                }
+            }
+
+
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun HomeScreenPreview() {
+    HomeScreen()
+}
